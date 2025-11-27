a5f3e0bed6943881642bbc083c34b2671bb4ddf8
0x00vb <valentinobalatti4@gmail.com>
2025-11-27 01:36:34 -0300

    fronend clean up

--- frontend/app/ledger/page.tsx
+++ frontend/app/ledger/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
 import ProtectedRoute from '../components/ProtectedRoute';
-import { WebSocketProvider } from '../contexts/WebSocketContext';
-import LedgerDashboard from '../components/ledger/LedgerDashboard';
-
 export default function LedgerPage() {
+
   return (
     <ProtectedRoute>
-      <WebSocketProvider>
-        <LedgerDashboard />
-      </WebSocketProvider>
+
     </ProtectedRoute>
   );
 }
